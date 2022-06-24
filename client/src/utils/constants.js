de36ca03fe76d324f2f2fb5a693f2b17e942cfc3
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2022-06-24 15:42:11 +0700

    Fix .env variable problem

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,5 @@
-export const CONTRACT_ADDRESS = '0x16C934C1eDF0F6c16907086BC7e840478B87d363';
-export const NETWORK_URL = 'https://rinkeby.etherscan.io/'
+console.log(process.env.REACT_APP_CONTRACT_ADDRESS)
+console.log(process.env.REACT_APP_NETWORK_URL)
+
+export const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS || '0x16C934C1eDF0F6c16907086BC7e840478B87d363';
+export const NETWORK_URL = process.env.REACT_APP_NETWORK_URL || 'https://rinkeby.etherscan.io/'
