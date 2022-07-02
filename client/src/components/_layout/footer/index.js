1da3a209fadffe33234183c19ac9cc5bb026b285
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2022-07-02 17:36:40 +0700

    Update footer info

--- client/src/components/_layout/footer/index.js
+++ client/src/components/_layout/footer/index.js
@@ -3,7 +3,7 @@ import { Box, Typography } from '@mui/material';
 export default function LayoutFooter() {
   return (
     <Box m={4} padding={2}>
-      <Typography>[CNM - 18_3]</Typography>
+      <Typography><b>[CNM - 18_3] Project Generate random number</b></Typography>
       <Typography>18120304 - 18120312 - 18120355</Typography>
     </Box>
   )
